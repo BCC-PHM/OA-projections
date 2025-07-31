f08a8a7d1e09728d17e9d4c8be3441ab911b1469
David.Ellis <david.ellis@birmingham.gov.uk>
2025-07-31 16:15:51 +0100

    Loaded mortality data and calculated rates

--- R/population_projection.R
+++ R/population_projection.R
@@ -3,20 +3,25 @@ library(readxl)
 library(dplyr)
 library(tidyr)
 library(data.table)
+library(ggplot2)
 
-# Load population data (ONS 2024 mid-year estimates)
+################################################################################
+#       Load and process population data (ONS 2023 mid-year estimates)         #
+################################################################################
+# Using 2023 instead of 2024 for better consistency with mortality data
+
 # https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/estimatesofthepopulationforenglandandwales
 pop_data_path <- file.path(pop_data_path_prefix,
-                           "Population EstimatesMID 2024 LA Level.xlsx")
+                           "Population EstimatesMID 2023 LA Level 230725.xlsx")
 
 # Define a function to process each sheet
 read_gender_sheet <- function(sheet_name) {
   read_excel(pop_data_path, sheet = sheet_name, skip = 7) %>%
     filter(Name == "Birmingham") %>%
     pivot_longer(cols = 5:95, names_to = "Age", values_to = "Count") %>%
-    mutate(Gender = gsub("MYE2 - ", "", sheet_name),
-           Gender = gsub("s", "", Gender)) %>%
-    select(Age, Count, Gender)
+    mutate(Sex = gsub("MYE2 - ", "", sheet_name),
+           Sex = gsub("s", "", Sex)) %>%
+    select(Age, Count, Sex)
 }
 
 # Read and combine male and female data
@@ -28,7 +33,7 @@ pop_grouped <- pop_est%>%
   # aggregate into 5 year bands
   mutate(
     Age = as.numeric(gsub("\\+", "", Age)),
-    Age_Group = cut(
+    AgeBand = cut(
       Age,
       breaks = seq(0, 95, 5),
       labels = c("0-4", "5-9", "10-14", "15-19", "20-24",
@@ -39,12 +44,44 @@ pop_grouped <- pop_est%>%
     )
   ) %>%
   group_by(
-    Gender, Age_Group
+    Sex, AgeBand
   ) %>%
   summarise(
-    Count = sum(Count),
+    Population = sum(Count),
     .groups = "drop"
   ) %>%
   mutate(
     Year = 2024
   )
+
+################################################################################
+#                      Load and process mortality data                         #
+################################################################################
+
+mort_data_path <- file.path(mortality_data_path_prefix,
+                           "deaths_by_year_sex_ageband_2019_2023_bham.csv")
+
+mort_data <- read.csv(mort_data_path) %>%
+  group_by(
+    Sex, AgeBand
+  ) %>%
+  summarize(
+    Deaths5Years = sum(Deaths),
+    .groups = "drop"
+  ) %>%
+  left_join(
+    pop_grouped,
+    by = join_by(Sex, AgeBand)
+  ) %>%
+  mutate(
+    p_hat = Deaths5Years / (5 * Population),
+    Value = 1e5 * p_hat,
+    a_prime = Deaths5Years + 1,
+    Z = qnorm(0.975),
+    LowerCI95 = 1e5 * Deaths5Years * (1 - 1/(9*Deaths5Years) - Z/3 * sqrt(1/a_prime))**3/(5 * Population),
+    UpperCI95 = 1e5 * a_prime * (1 - 1/(9*a_prime) + Z/3 * sqrt(1/a_prime))**3/(5 * Population)
+  )
+
+
+
+
