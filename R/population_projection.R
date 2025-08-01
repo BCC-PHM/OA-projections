3f1f2aef2a42a301437fa0ad17c1f42f704a7ce5
David.Ellis <david.ellis@birmingham.gov.uk>
2025-08-01 18:37:00 +0100

    Completed basic population projection

--- R/population_projection.R
+++ R/population_projection.R
@@ -51,7 +51,11 @@ pop_grouped <- pop_est%>%
     .groups = "drop"
   ) %>%
   mutate(
-    Year = 2024
+    Year = 2024,
+    AgeBandSortable = as.numeric(sub("^([0-9]+).*", "\\1", AgeBand))
+  ) %>%
+  select(
+    Sex, AgeBand, AgeBandSortable, Population, Year
   )
 
 ################################################################################
@@ -83,11 +87,15 @@ mort_rates <- read.csv(mort_data_path) %>%
   mutate(
     AgeBand = factor(AgeBand, levels = unique(pop_grouped$AgeBand)),
     p_hat = Deaths5Years / (NumYears * Population),
-    MortRate = 1e5 * p_hat,
+    MortRate =  p_hat,
     a_prime = Deaths5Years + 1,
     Z = qnorm(0.975),
-    MortRateLowerCI95 = 1e5 * Deaths5Years * (1 - 1/(9*Deaths5Years) - Z/3 * sqrt(1/a_prime))**3/(NumYears * Population),
-    MortRateUpperCI95 = 1e5 * a_prime * (1 - 1/(9*a_prime) + Z/3 * sqrt(1/a_prime))**3/(NumYears * Population)
+    MortRateLowerCI95 = Deaths5Years * (1 - 1/(9*Deaths5Years) - Z/3 * sqrt(1/a_prime))**3/(NumYears * Population),
+    MortRateUpperCI95 = a_prime * (1 - 1/(9*a_prime) + Z/3 * sqrt(1/a_prime))**3/(NumYears * Population),
+    AgeBandSortable = as.numeric(sub("^([0-9]+).*", "\\1", AgeBand))
+  ) %>%
+  select(
+    Sex, AgeBand, AgeBandSortable, MortRate, MortRateLowerCI95, MortRateUpperCI95
   )
 
 
@@ -98,10 +106,10 @@ mort_rates <- read.csv(mort_data_path) %>%
 male_color <- "#9657E0"
 female_color <- "#1DAA47"
 
-ggplot(mort_rates, aes(x = AgeBand, y = MortRate, fill = Sex)) +
+ggplot(mort_rates, aes(x = AgeBand, y = 1e5*MortRate, fill = Sex)) +
   geom_col(position = "dodge") +
   geom_errorbar(
-    aes(x = AgeBand, ymin = MortRateLowerCI95, ymax = MortRateUpperCI95),
+    aes(x = AgeBand, ymin = 1e5*MortRateLowerCI95, ymax = 1e5*MortRateUpperCI95),
     width = 0.5,
     position = position_dodge(0.9),
     ) +
@@ -123,4 +131,102 @@ ggplot(mort_rates, aes(x = AgeBand, y = MortRate, fill = Sex)) +
 
 ggsave("output/mortality_rates_19to23.png", width = 8, height = 4)
 
+################################################################################
+#                        Project populations                                   #
+################################################################################
 
+estimate_next_five <- function(
+  current_population,
+  mortality_rates
+) {
+  
+  # Get new year
+  next_year <- unique(current_population$Year) + 5
+  print(Year)
+  # Get 0 to 4 population to propagate births
+  pop_0to4 <- current_population %>%
+    filter(AgeBand == "0-4")
+    
+  
+  # Join mortality
+  joined <- current_population %>%
+    left_join(
+      mortality_rates,
+      by = join_by(Sex, AgeBand, AgeBandSortable)
+    ) %>%
+    arrange(
+      Sex, AgeBandSortable
+    )
+  
+  # Apply mortality
+  pop_after_deaths <- joined %>%
+    mutate(
+      Population = Population * (1 - 5 * MortRate),
+    )
+  
+  age_groups <- levels(joined$AgeBand)
+  
+  shift_lookup <- setNames(
+    c(age_groups[-1], "90+"),  # Destination (note last one repeats)
+    age_groups                 # Current
+  )
+
+  # Apply the shift
+  aged_up <- pop_after_deaths %>%
+    mutate(
+      AgeBand = shift_lookup[as.character(AgeBand)]
+    ) %>%
+    group_by(
+      Sex, AgeBand
+      ) %>%
+    summarise(
+      Population = sum(Population), 
+      .groups = "drop"
+      ) %>%
+    mutate(
+      AgeBandSortable = as.numeric(sub("^([0-9]+).*", "\\1", AgeBand)),
+      Year := next_year
+      ) %>%
+    select(
+      Sex, AgeBand, AgeBandSortable, Population, Year
+    ) %>%
+    rbind(
+      pop_0to4
+    ) %>%
+    mutate(
+      Year := next_year
+    ) %>%
+    arrange(
+      Sex, AgeBandSortable
+    )
+  aged_up$AgeBand <- as.factor(aged_up$AgeBand)
+  
+  return(aged_up)
+  
+}
+ 
+prop_proj_list <- list("2023" = pop_grouped)
+ 
+for (Year in seq(2028, 2053, 5)) {
+  prop_proj_list[[as.character(Year)]] <- estimate_next_five(
+    prop_proj_list[[as.character(Year - 5)]],
+    mort_rates
+  )
+}
+
+pop_proj <- rbindlist(prop_proj_list)
+
+proj_plot <- pop_proj %>% 
+  filter(AgeBandSortable > 45) %>%
+  ggplot(aes(x = Year, y = Population)) +
+  geom_line() +
+  theme_bw() +
+  facet_grid(
+    rows = vars(AgeBand), 
+    cols = vars(Sex),
+    scales="free_y"
+  )
+proj_plot
+
+ggsave("output/basic-projection.png",
+       proj_plot, width = 5, height = 8)
